7ed149c01410a98662707ba4882953eaa3b3a38b
Rishavbagga77 <41142557+Rishavbagga77@users.noreply.github.com>
2024-05-07 12:58:47 +0530

    adding triggers

--- lambda-east.tf
+++ lambda-east.tf
@@ -12,6 +12,12 @@ provider "aws" {
   region = "us-gov-east-1"
 }
 
+variable "invoke" {
+  type = list(string)
+  default = ["arn:aws-us-gov:s3:::lifebit-logging-govcloud"]
+  #default = ["arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026597159496/vpcflowlogs/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026592988471/vpcflowlogs/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026589913916/vpcflowlogs/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026557756475/vpcflowlogs/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026536877604/GuardDuty/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026553734564/GuardDuty/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026557756475/GuardDuty/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026592988471/GuardDuty/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026597159496/GuardDuty/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026592988471/network-firewall/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026589913916/network-firewall/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026557756475/network-firewall/"]
+}
+
 #to collect aws region and account_id to create arns
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
@@ -28,20 +34,30 @@ resource "aws_cloudformation_stack" "datadog_forwarder" {
   capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
   parameters   = {
     DdApiKeySecretArn  = var.secret_arn,
-    DdSite             = "app.ddog-gov.com",
-    FunctionName       = "datadog-forwarder-tf"
+    DdSite             = "ddog-gov.com",
+    FunctionName       = "datadog-forwarder-tf-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
   }
   template_url = "https://datadog-cloudformation-template.s3.amazonaws.com/aws/forwarder/latest.yaml"
 }
 
-#Adding Trigger to Datadog Lambda
+#Adding invokes to Datadog Lambda
 resource "aws_lambda_permission" "allow_bucket" {
-  statement_id  = "AllowExecutionFromS3Bucket"
+  for_each = toset(var.invoke)
   action        = "lambda:InvokeFunction"
-  function_name = join(":", ["arn:aws","lambda",data.aws_region.current.name,data.aws_caller_identity.current.account_id,"function","datadog-forwarder-tf"])
+  function_name = join(":", ["arn:aws-us-gov","lambda",data.aws_region.current.name,data.aws_caller_identity.current.account_id,"function","datadog-forwarder-tf-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"])
   principal     = "s3.amazonaws.com"
-  source_arn    = ["arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026597159496/vpcflowlogs/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026592988471/vpcflowlogs/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026589913916/vpcflowlogs/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026557756475/vpcflowlogs/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026536877604/GuardDuty/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026553734564/GuardDuty/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026557756475/GuardDuty/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026592988471/GuardDuty/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026597159496/GuardDuty/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026592988471/network-firewall/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026589913916/network-firewall/", "arn:aws-us-gov:s3:::lifebit-logging-govcloud/AWSLogs/026557756475/network-firewall/"] 
+  source_arn    = each.value
   depends_on = [
     aws_cloudformation_stack.datadog_forwarder
   ]
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = "lifebit-logging-govcloud"
+  lambda_function {
+    lambda_function_arn = join(":", ["arn:aws-us-gov","lambda",data.aws_region.current.name,data.aws_caller_identity.current.account_id,"function","datadog-forwarder-tf-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"])
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "AWSLogs/"
+  }
+  depends_on = [aws_lambda_permission.allow_bucket]
 }
